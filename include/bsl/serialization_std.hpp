1a584bf64d4a10a82cdafe93266c8a7f116d1e8d
I.Kostiuk <gefestroyale@gmail.com>
2024-06-01 18:28:10 +0300

    Add serialization for map

--- include/bsl/serialization_std.hpp
+++ include/bsl/serialization_std.hpp
@@ -3,6 +3,7 @@
 #include "bsl/serialization.hpp"
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 template<>
 struct Serializer<std::string>{
@@ -28,7 +29,6 @@ struct Serializer<std::string>{
 
 template<typename T>
 struct Serializer<std::vector<T>>{
-
 	static void ToStream(const std::vector<T>& vector, std::ostream& stream) {
 		Serializer<std::size_t>::ToStream(vector.size(), stream);
 
@@ -55,5 +55,52 @@ struct Serializer<std::vector<T>>{
 
 		return {std::move(vector)};
 	}
+};
 
+template<typename F, typename S>
+struct Serializer<std::pair<F, S>>{
+	static void ToStream(const std::pair<F, S>& pair, std::ostream& stream) {
+		Serializer<F>::ToStream(pair.first, stream);
+		Serializer<S>::ToStream(pair.second, stream);
+	}
+
+	static std::optional<std::pair<F, S>> FromStream(std::istream& stream) {
+		auto first  = Serializer<F>::FromStream(stream);
+		auto second = Serializer<S>::FromStream(stream);
+		
+		if(!first.has_value() || !second.has_value())
+			return std::nullopt;
+
+		return {{std::move(first.value()), std::move(second.value())}};
+	}
+};
+
+template<typename KeyType, typename ValueType>
+struct Serializer<std::unordered_map<KeyType, ValueType>>{
+	static void ToStream(const std::unordered_map<KeyType, ValueType>& map, std::ostream& stream) {
+		Serializer<std::size_t>::ToStream(map.size(), stream);
+
+		for (const auto& pair : map)
+			Serializer<std::pair<KeyType, ValueType>>::ToStream(pair, stream);
+	}
+
+	static std::optional<std::unordered_map<KeyType, ValueType>> FromStream(std::istream& stream) {
+		auto size = Serializer<std::size_t>::FromStream(stream);
+
+		if(!size.has_value())
+			return std::nullopt;
+	
+		std::unordered_map<KeyType, ValueType> map;
+
+		for(int i = 0; i<size.value(); i++) {
+			auto pair = Serializer<std::pair<KeyType, ValueType>>::FromStream(stream);
+
+			if(!pair.has_value())
+				return std::nullopt;
+			
+			map.emplace(std::move(pair.value()));
+		}
+		
+		return {std::move(map)};
+	}
 };
